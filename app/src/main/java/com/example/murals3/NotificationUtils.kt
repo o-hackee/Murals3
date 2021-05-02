b56dd3920799c2920635844bee0e0696a4fa6fe6
olga <korotkovaov@gmail.com>
2021-05-02 17:51:36 +0200

    coordinates adjusted; unique notification ids to allow multiple simultaneously

--- app/src/main/java/com/example/murals3/NotificationUtils.kt
+++ app/src/main/java/com/example/murals3/NotificationUtils.kt
@@ -21,11 +21,10 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.graphics.BitmapFactory
 import android.graphics.Color
-import android.os.Build
 import androidx.core.app.NotificationCompat
 import com.example.murals3.MapsActivity.Companion.ACTION_GEOFENCE_NOTIFY_EVENT
+import timber.log.Timber
 
 /*
  * We need to create a NotificationChannel associated with our CHANNEL_ID before sending a
@@ -50,9 +49,11 @@ fun NotificationManager.sendGeofenceEnteredNotification(context: Context, foundI
     val contentIntent = Intent(context, MapsActivity::class.java)
     contentIntent.action = ACTION_GEOFENCE_NOTIFY_EVENT
     contentIntent.putExtra(GeoDataModel.EXTRA_GEOFENCE_INDEX, foundIndex)
+    val notificationId = System.currentTimeMillis().toInt()
+    Timber.i("notificationId: $notificationId")
     val contentPendingIntent = PendingIntent.getActivity(
         context,
-        NOTIFICATION_ID,
+        notificationId,
         contentIntent,
         PendingIntent.FLAG_UPDATE_CURRENT
     )
@@ -68,7 +69,7 @@ fun NotificationManager.sendGeofenceEnteredNotification(context: Context, foundI
         .setAutoCancel(true)
         .setPriority(NotificationCompat.PRIORITY_HIGH)
 
-    notify(NOTIFICATION_ID, builder.build())
+    notify(notificationId, builder.build())
 }
 
 private const val NOTIFICATION_ID = 33
